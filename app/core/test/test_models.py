5ee35646e0d08b68001611244b2cce4b31ddd6c5
miguel <miguel@miguel-MS-7C91>
2020-08-13 17:14:59 -0500

    Added new test to custom user models.
    Added validation for super user create, also added validation for None email values when creating users

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -22,3 +22,19 @@ class ModelTest(TestCase):
         user = get_user_model().objects.create_user(email,'test1234')
 
         self.assertEqual(user.email,email.lower())
+    
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        # Notice, everything inside this block will return a value error
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
+    
+    def test_create_new_super_user(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@aguacate.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
