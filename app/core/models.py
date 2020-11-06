4e01f184fff1738a2b1e4db96d29f02a972f58e6
sandoval19 <miiguel.correea@hotmail.com>
2020-11-06 00:14:35 -0500

    Fixed minminal db names at Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -10,7 +10,7 @@ class UserManager(BaseUserManager):
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password=password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
