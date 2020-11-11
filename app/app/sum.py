f6d3b11b62cc9bef5ba390cf9c0713fb6531b199
sandoval19 <miiguel.correea@hotmail.com>
2020-11-10 21:59:09 -0500

    Fixed spaces, endlines, comments structure. Delete un used models and test.

--- app/app/sum.py
+++
@@ -1,7 +0,0 @@
-def add(x, y):
-    """Return two number added"""
-    return x + y
-
-def subtract(x, y):
-    """Return two number subtraction"""
-    return y - x
--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.urls import reverse # allows to call urls of the project
-from django.test import Client # allows to make test requests to the app
+# allows to call urls of the project
+from django.urls import reverse
+# allows to make test requests to the app
+from django.test import Client
 
 
 class AdminSiteTests(TestCase):
 
     def setUp(self):
-        self.client = Client() # Creates connection to attempt request
+        # Creates connection to attempt request
+        self.client = Client()
         # Create super user and login
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@aguacate.com',
@@ -32,19 +35,17 @@ class AdminSiteTests(TestCase):
         # also "checks" if the http response code is 200/400
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-    
+
     def test_user_change_page(self):
         """ Test that the user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
         # test that the web page renders ok
         self.assertEqual(res.status_code, 200)
-    
+
     def test_create_user_page(self):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
-
         self.assertEqual(res.status_code, 200)
-
