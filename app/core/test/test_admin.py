cf56c590af6b5718dd991f227f216bb3b3eca7dd
sandoval19 <miiguel.correea@hotmail.com>
2020-11-10 21:46:33 -0500

    Updated admin to support custom user model based on email

--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -32,4 +32,19 @@ class AdminSiteTests(TestCase):
         # also "checks" if the http response code is 200/400
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+    
+    def test_user_change_page(self):
+        """ Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        # test that the web page renders ok
+        self.assertEqual(res.status_code, 200)
+    
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+
+        self.assertEqual(res.status_code, 200)
 
